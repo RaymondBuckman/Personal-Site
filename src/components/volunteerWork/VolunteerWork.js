19090f62bdc1fdd190cb0b274e77b5d13c6f187e
Raymond <dude92128@aol.com>
2017-11-03 23:52:27 -0700

    Video window added

--- src/components/volunteerWork/VolunteerWork.js
+++ src/components/volunteerWork/VolunteerWork.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import cameraVideo from '../../images/cameravideo.mp4';
 
 
 class VolumteerWork extends Component {
@@ -8,10 +9,14 @@ class VolumteerWork extends Component {
             <div id="jump-to-vWork" className="VWork-1">
                     <ul className="flex-container">
                         <li className="flex-item">
-                            <div className="scrollmagicWindow window2"></div>
+                            <div className="scrollmagicWindow window2">
+                                <video preload="preload" id="volunteer-video" autoPlay="autoplay" loop="loop" muted>
+                                    <source src={cameraVideo} type="video/mp4"></source>
+                                </video>
+                            </div>
                         </li>
                         <li className="flex-item">
-                            <header id="vWork"  className="scrollmagicFadeIn">Volunteer Work</header>
+                            <header id="vWork"  className="scrollmagicFadeIn" tabIndex="0">Volunteer Work</header>
                             <ul id="vList" className="scrollmagicFadeIn">
                                 <li>Cameraman</li><br/>
                                 <li>Average of 2 hours/week since February 2016</li><br/>
