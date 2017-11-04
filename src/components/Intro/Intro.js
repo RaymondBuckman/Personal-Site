663112eca2c08170d62d2f24e574b1c0372fa338
Raymond <dude92128@aol.com>
2017-11-03 23:51:58 -0700

    Video window added

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
 import ReactTooltip from 'react-tooltip';
+import subwayVideo from '../../images/subwayvideo.mp4';
 
 class Intro extends Component {
     render() {
         return(
         <div> 
             <div className="Intro-1" id="top-of-page">
-                        <div id="name">
+                        <header id="name">
                             <span id="name_R">R</span><span id="name_a">a</span><span id="name_y">y</span><span id="name_m">m</span><span id="name_o">o</span><span id="name_nd">nd</span>
                             <span id="name_B">B</span><span id="name_uc">uc</span><span id="name_k">k</span>ma<span id="name_n">n</span>
-                        </div>
-                        <div className="col-md-4 col-md-offset-4 text-center">
-                            <span id="down-arrow" className="glyphicon glyphicon-menu-down" data-tip data-for="scroll" aria-describedby="scroll"></span>
-                        </div>
+                        </header>
+                        <img id="down-arrow" src="https://png.icons8.com/downward-arrow/ios7/73/000000" height="60px" data-tip data-for="scroll" aria-describedby="scroll"></img>            
             </div>
             
             
             <div className="Intro-2" id="arrow-dest">
                 <ul className="flex-container">
                     <li className="flex-item">
-                        <div className="scrollmagicWindow window1" id="window-1"></div>
+                        <div className="scrollmagicWindow window1" id="window-1">
+                            <video preload="preload" id="subway-video" autoPlay="autoplay" loop="loop" muted>
+                                <source src={subwayVideo} type="video/mp4"></source>
+                            </video> 
+                        </div>
                     </li>
                     <li className="flex-item">
-                        <h1 id="about-me" className="scrollmagicFadeIn">Ab<span id="aboutMe_o">o</span><span id="aboutMe_ut">ut</span> <span id="aboutMe_M">M</span><span id="aboutMe_e">e</span>
+                        <h1 id="about-me" className="scrollmagicFadeIn" tabIndex="0">Ab<span id="aboutMe_o">o</span><span id="aboutMe_ut">ut</span> <span id="aboutMe_M">M</span><span id="aboutMe_e">e</span>
                         </h1><br/>
                         <p id="about-me-paragraph" className="scrollmagicFadeIn">I am an enthusiastic, driven college graduate looking for a rewarding entry-level position in tech. Currently, I am seeking employment opportunities that will both challenge me &
                         allow me to grow within the company. Specific fields that are of interest to me are web development, software-engineering and QA. The phrase "learn something new every day" is something that I truly take to heart. I have a passion for acquiring new knowledge both relating to my career path & about the world around me. I consider myself a citizen of the world & would be completely open to opportunities abroad. People who know me would say that I am a pleasure to work with. I am a great team-player who can meet deadlines & exceed expectations. I am also keeping my skill set up-to-date, as well as learning new languages, both coding & spoken. Feel free to reach out if you're interested! </p>
@@ -39,6 +42,7 @@ class Intro extends Component {
                         <li><a href="#jump-to-vWork">Volunteer Work</a></li>
                         <li><a href="#jump-to-hobbies">Hobbies</a></li>
                     </ul>
+                    <img id="nav-icon" height="40px"></img>
                 </div>
             </nav>
             
