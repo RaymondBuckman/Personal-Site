61b4b0b5f312df6b955a2960fc8d4a8bcd14b64b
Raymond <dude92128@aol.com>
2017-11-04 23:58:11 -0700

    Changed header + improved semantics

--- src/components/languages/Languages.js
+++ src/components/languages/Languages.js
@@ -6,18 +6,18 @@ class Languages extends Component {
         return(
         <div>
             <div id="jump-to-languages" className="Languages-1">
-                <header id="languages" className="scrollmagicFadeIn" tabIndex="0">
-                    <span id="languages_L">L</span><span id="languages_an">an</span><span id="languages_gu">gu</span><span id="languages_ag">ag</span><span id="languages_e">e</span><span id="languages_s">s</span>
+                <header id="languages" className="scrollmagic-fade-in" tabIndex="0">
+                    Lan<span id="languages_gu">gu</span>ag<span id="languages_e">e</span>s & L<span id="languages_ib">ib</span>raries
                 </header>
-                <ul id="language-list"  className="scrollmagicFadeIn">
+                <ul id="language-list"  className="scrollmagic-fade-in">
                     <li>HTML5/CSS3</li><br/>
                     <li>Javascript</li><br/>
                     <li>JQuery</li><br/>
-                    <li>Bootstrap</li><br/>
-                    <li>Sass/Grunt</li><br/>
+                    <li>Bootstrap 3 & 4</li><br/>
+                    <li>Sass (Scss)</li><br/>
                     <li>React.js</li><br/>
-                    <li>Webpack</li><br/>
-                    <li>Java/mySQL</li>
+                    <li>Java</li><br/>
+                    <li>mySQL</li>
                 </ul>
             </div> 
             <div className="Languages-2"></div>
