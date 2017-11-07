df1c1f23515ac4374af95ea48613599cc8dca249
Raymond <dude92128@aol.com>
2017-11-06 23:00:26 -0800

    Update Hobbies.js

--- src/components/hobbies/Hobbies.js
+++ src/components/hobbies/Hobbies.js
@@ -10,51 +10,51 @@ class Hobbies extends Component {
                         <h1 id="hobbies" className="scrollmagic-fade-in" tabIndex="0">
                             Ho<span id="hobbies_bb">bb</span>ies
                         </h1></li>
-                    <li className="flex-item">
+                    <li className="flex-item scrollmagic-circle">
                         <div></div>
                         <h2>Tennis</h2>
                     </li>
-                    <li className="flex-item">
+                    <li className="flex-item scrollmagic-circle">
                         <div></div>
                         <h2>Trance & Music production</h2>
                     </li>
-                    <li className="flex-item">
+                    <li className="flex-item scrollmagic-circle">
                         <div></div>
                         <h2>Travel</h2>
                     </li>
-                    <li className="flex-item">
+                    <li className="flex-item scrollmagic-circle">
                         <div></div>
                         <h2>Foreign Languages</h2>
                     </li>
-                    <li className="flex-item">
+                    <li className="flex-item scrollmagic-circle">
                         <div></div>
                         <h2>Weight-Lifting</h2>
                     </li>
-                    <li className="flex-item">
+                    <li className="flex-item scrollmagic-circle">
                         <div></div>
                         <h2>Running</h2>
                     </li>
-                    <li className="flex-item">
+                    <li className="flex-item scrollmagic-circle">
                         <div></div>
                         <h2>Cars & Driving</h2>
                     </li>
-                    <li className="flex-item">
+                    <li className="flex-item scrollmagic-circle">
                         <div></div>
                         <h2>Ping Pong</h2>
                     </li>
-                    <li className="flex-item">
+                    <li className="flex-item scrollmagic-circle">
                         <div></div>
                         <h2>Geography & History</h2>
                     </li>
-                    <li className="flex-item">
+                    <li className="flex-item scrollmagic-circle">
                         <div></div>
                         <h2>Health & Nutrition</h2>
                     </li>
-                    <li className="flex-item">
+                    <li className="flex-item scrollmagic-circle">
                         <div></div>
                         <h2>Pets</h2>
                     </li>
-                    <li className="flex-item">
+                    <li className="flex-item scrollmagic-circle">
                         <div></div>
                         <h2>Gaming</h2>
                     </li>
