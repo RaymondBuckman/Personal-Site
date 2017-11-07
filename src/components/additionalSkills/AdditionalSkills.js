26c2bc5326213bef6d7df9af7de8181b23f61d65
Raymond <dude92128@aol.com>
2017-11-06 22:59:45 -0800

    Update AdditionalSkills.js

--- src/components/additionalSkills/AdditionalSkills.js
+++ src/components/additionalSkills/AdditionalSkills.js
@@ -10,6 +10,8 @@ class AdditionalSkills extends Component {
                     Add<span id="additional-skills_iti">iti</span>onal Ski<span id="additional-skills_ll">ll</span>s
                 </header>
                 <ul id="skill-list"  className="scrollmagic-fade-in">
+                    <li>Webpack3/Grunt</li><br/>
+                    <li>Node.js</li><br/>
                     <li>Atom/Brackets</li><br/>
                     <li>Notepad++</li><br/>
                     <li>MS Visual Studio</li><br/>
@@ -17,23 +19,13 @@ class AdditionalSkills extends Component {
                     <li>Git & GitHub</li><br/>
                     <li>Microsoft Office</li><br/>
                     <li>Adobe Photoshop</li><br/>
-                    <li>Français (intermédiaire)</li><br/>
-                    <li>Deutsch (anfänger)</li>
                 </ul>
             </div>
             <div className="Skills-2">
-                <div className="container">
-                    <div className="row">
-                        <header id="french" className="col-sm-2.5 col-sm-offset-1 scrollmagic-fade-in" data-toggle="tooltip" data-delay="400" data-animation="true" data-easein="perspectiveLeftIn" data-placement="right" title="Mon niveau de compétence: Intermédiaire">Bon travail!</header>
-                    </div> 
-                </div>
+                        <header id="french" className="scrollmagic-fade-in">Bon travail!</header>
             </div>
             <div className="Skills-3">
-                <div className="container">
-                    <div className="row">
-                        <header id="german" className="col-xs-2.5 col-xs-offset-9 scrollmagic-fade-in" data-toggle="tooltip" data-delay="400" data-animation="true" data-easein="perspectiveRightIn"  data-placement="left" title="Mein Niveau:Anfänger">Ausgezeichnet!</header>
-                    </div>
-                </div>
+                        <header id="german" className="scrollmagic-fade-in"> Ausgezeichnet!</header>
             </div>
         </div>
         );
