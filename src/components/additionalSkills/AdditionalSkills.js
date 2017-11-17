ac4fc4519d4feea556414a95417b401a48e9ced3
Raymond <dude92128@aol.com>
2017-11-16 23:54:12 -0800

    Update AdditionalSkills.js

--- src/components/additionalSkills/AdditionalSkills.js
+++ src/components/additionalSkills/AdditionalSkills.js
@@ -25,7 +25,7 @@ class AdditionalSkills extends Component {
             {/*<p>Salut! En plus de apprendre des nouveaux langages de programmation, j'apprends aussi des nouvelles langues parlées. Je parle français à un niveau intermédiaire. J'ai eu plusiers de cours de français au lycée, et apres un peu d'annees j'ai decide l'apprendre en permanence. Je peux lire et ecrire mieux que peux parler, mais je m'améliore chaque jour!</p>*/}
             </div>
             <div className="Skills-3">
-            {/*<p>Hallo! Ich spreche auch ein bisschen Deutsch. Mein Deutsch ist nicht so gut als mein Französisch. Meine Interesse am Autos ist d!</p>*/}
+            {/*<p>Hallo! Ich spreche auch ein bisschen Deutsch. Mein Deutsch ist nicht so gut als mein Französisch.</p>*/}
             </div>
         </div>
         );
