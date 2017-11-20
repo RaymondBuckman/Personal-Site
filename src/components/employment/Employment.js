707da8be5ec623d1fb2ac416efe333a61df97643
Raymond <dude92128@aol.com>
2017-11-19 23:58:12 -0800

    Update Employment.js

--- src/components/employment/Employment.js
+++ src/components/employment/Employment.js
@@ -16,7 +16,7 @@ class Employment extends Component {
                     </li>
                     <li className="flex-item">
                         <h1 id="employment" className="scrollmagic-fade-in" tabIndex="0">
-                            Empl<span id="employment_oy">oy</span>ment
+                            Empl<span id="employment_oy">oy</span>me<span id="employment_n">n</span>t
                         </h1><br/>
                         <p id="employment-paragraph" className="scrollmagic-fade-in">What have I been doing since graduation? Everything you see on HGTV! Painting, demolition, kitchen design, installing back splash, managing workers, pest control, installing cabinets...you name it, I've done it. What does this have to do with front end web develoment, you ask? Nothing directly, but it shows I can work long hours, perform under pressure, communicate well, be decisive, & get 'er done! I've also done a brief stint as an exhibitor at ARM TechCon. </p>
                     </li>
