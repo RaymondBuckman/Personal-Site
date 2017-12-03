897cce76cfd3bfef27b77f48fbafc05725d74df4
Raymond <dude92128@aol.com>
2017-12-02 23:50:19 -0800

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,9 @@ class Footer extends Component {
                         <header id="thanks">Thanks for visiting!</header>
                     </div>
                     <div className="col-xs-4 text-center">
-                        <h1 id="fin" className="scrollmagic-fade-in" tabIndex="0">Fin</h1>
+                        <h1 id="fin" className="scrollmagic-fade-in" tabIndex="0">
+                            <span id="fin-F">F</span><span id="fin-i">i</span>n
+                        </h1>
                     </div>
                     <div className="col-xs-4 text-center">
                         <a id="icons8" href="https://icons8.com" target="_blank">Icon pack By Icons8</a>
