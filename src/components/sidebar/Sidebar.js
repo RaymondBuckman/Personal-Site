b1eb7aecb16ebb514fb1a8b0d6409ac63693fdf9
Raymond <dude92128@aol.com>
2017-12-12 23:35:56 -0800

    Update Sidebar.js

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import linkedIn from '../../images/linkedin-icon.png';
+import github from '../../images/github-icon.png';
+import icons8 from '../../images/icons8-icon.png';
+import pexels from '../../images/pexels-icon.png';
 import $ from "jquery";
 
 class Sidebar extends Component {
@@ -14,22 +18,22 @@ class Sidebar extends Component {
                 </div>                
                 <li>
                     <a href="https://www.linkedin.com/in/raymondbuckman" target="_blank">
-                        <span className="sidebar-first-letter">L</span>inked In<span className="pull-right"><img src="https://png.icons8.com/linkedin/color/96" title="LinkedIn" width="30" height="30"></img></span>
+                        <span className="sidebar-first-letter">L</span>inked In<span className="pull-right"><img src={linkedIn} title="LinkedIn" width="30" height="30"></img></span>
                     </a>
                 </li>
                 <li>
                     <a href="https://github.com/RaymondBuckman" target="_blank">
-                        <span className="sidebar-first-letter">G</span>ithub<span className="pull-right"><img id="github-logo" src="https://png.icons8.com/github/win10/96" title="GitHub" width="30" height="30"></img></span>
+                        <span className="sidebar-first-letter">G</span>ithub<span className="pull-right"><img id="github-logo" src={github} title="GitHub" width="30" height="30"></img></span>
                     </a>
                 </li>
                 <li>
                     <a href="https://icons8.com/" target="_blank">
-                        <span className="sidebar-first-letter">I</span>cons8<span className="pull-right"><img id="icons8-logo" src="https://png.icons8.com/icons8-filled/ios7/100" title="Icons8 Filled" width="30" height="30"></img></span>
+                        <span className="sidebar-first-letter">I</span>cons8<span className="pull-right"><img id="icons8-logo" src={icons8} title="Icons8 Filled" width="30" height="30"></img></span>
                     </a>
                 </li>
                 <li>
                     <a href="https://www.pexels.com/" target="_blank">
-                        <span className="sidebar-first-letter">P</span>exels<span className="pull-right"><img id="icons8-logo" src="https://png.icons8.com/camera/ios11/73/000000" title="Icons8 Filled" width="30" height="30"></img></span>
+                        <span className="sidebar-first-letter">P</span>exels<span className="pull-right"><img id="icons8-logo" src={pexels} title="Icons8 Filled" width="30" height="30"></img></span>
                     </a>
                 </li>
             </ul>
