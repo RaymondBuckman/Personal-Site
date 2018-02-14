812c141fca5128d996e3ecf325b269c74569cf9c
Raymond <dude92128@aol.com>
2018-02-13 22:52:46 -0800

    Update Hobbies.js

--- src/components/hobbies/Hobbies.js
+++ src/components/hobbies/Hobbies.js
@@ -29,49 +29,49 @@ export default class Hobbies extends Component {
                             Hob<span id="hobbies-b">b</span>ies & In<span id="hobbies-t">t</span>ere<span id="hobbies-s">s</span>ts
                         </h1></li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={tennis}></img>
+                        <img src={tennis} alt="David Ferrer hitting a forehand on a clay court"></img>
                         <span className="text-container">
                             <h2>Tennis</h2>
                             <p>Favorite players:<br/>Rafael Nadal<br/>Stan Wawrinka<br/>David Ferrer (pictured)<br/>Venus Williams<br/>Simona Halep</p>
                         </span>
                     </li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={trance}></img>
+                        <img src={trance} alt="DJs performing at a music festival"></img>
                         <span className="text-container">
                             <h2>Trance</h2>
                             <p>What I'm listening to:<br/>Sail - Armin van Buuren<br/>Polygons - Tomac & Mad<br/>Musica - Giuseppe Ottaviani<br/>Ondine - Nhato</p>
                         </span>
                     </li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={travel}></img>
+                        <img src={travel} alt="An airplane flying into the horizon"></img>
                         <span className="text-container">
                             <h2>Travel</h2>
                             <p>Bucket list:<br/>Croatia<br/>Turkey<br/>Japan<br/>Switzerland<br/>Georgia</p>
                         </span>
                     </li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={foreignLanguages}></img>
+                        <img src={foreignLanguages} alt="Black Chinese script written on white wooden planks"></img>
                         <span className="text-container">
                             <h2>Foreign Languages</h2>
                             <p>Вы можете читать это? Я могу! Check out my <a id="duolingo" href="https://www.duolingo.com/xKrankenhausx" target="_blank" tabIndex="0">Duolingo</a> account</p>
                         </span>
                     </li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={weightLifting}></img>
+                        <img src={weightLifting} alt="Man lifting a heavy barbell"></img>
                         <span className="text-container">
                             <h2>Weight-Lifting</h2>
                             <p>Mind over matter</p>
                         </span>
                     </li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={running}></img>
+                        <img src={running} alt="Man running on a treadmill"></img>
                         <span className="text-container">
                             <h2>Running</h2>
                             <p>Treadmill hog</p>
                         </span>
                     </li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={cars}></img>
+                        <img src={cars} alt="The Audi R18 e-tron quattro at Le Mans"></img>
                         <span className="text-container">
                             <h2>Cars & Auto Racing</h2>
                             <p>Favorite brands: Porsche, Audi, Mazda, McLaren</p>
@@ -79,77 +79,77 @@ export default class Hobbies extends Component {
                         </span>
                     </li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={pingPong}></img>
+                        <img src={pingPong} alt="Two ping pong paddles & a ball laying on a ping pong table"></img>
                         <span className="text-container">
                             <h2>Ping Pong</h2>
                             <p>#KillerBackhand</p>
                         </span>
                     </li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={geography}></img>
+                        <img src={geography}  alt="Map of Africa & Eurasia"></img>
                         <span className="text-container">
                             <h2>Geography</h2>
                             <p>Geography Now<br/>#NotificationSquad</p>
                         </span>
                     </li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={health}></img>
+                        <img src={health} alt="Vine of tomatoes & carrots on a table"></img>
                         <span className="text-container">
                             <h2>Health & Nutrition</h2>
                             <p>You are what you eat!</p>
                         </span>
                     </li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={pets}></img>
+                        <img src={pets} alt="A white guinea pig"></img>
                         <span className="text-container">
                             <h2>Pets</h2>
                             <p>I love dogs & guinea pigs!</p>   
                         </span>
                     </li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={gaming}></img>
+                        <img src={gaming} alt="A black Xbox One controller"></img>
                         <span className="text-container">
                             <h2>Gaming</h2>
                             <p>Favorite games:<br/>Metroid Prime 1-3<br/>Killer Instinct<br/>MK9/MKX<br/>Injustice 2<br/>Skyrim<br/>Super Smash Bros. Melee</p>
                         </span>
                     </li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={musicProduction}></img>
+                        <img src={musicProduction} alt="FruityLoops screenshot"></img>
                         <span className="text-container">
                             <h2>Music Production</h2>
                             <p>FL Studio novice</p>
                         </span>
                     </li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={history}></img>
+                        <img src={history} alt="Painting of Ivan the Terrible"></img>
                         <span className="text-container">
                             <h2>History</h2>
                             <p>Consumer of Knowledge</p>
                         </span>
                     </li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={technology}></img>
+                        <img src={technology} alt="Close-up of a circuitboard"></img>
                         <span className="text-container">
                             <h2>Technology</h2>
                             <p>Vorsprung Durch Technik</p>
                         </span>
                     </li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={modernDesign}></img>
+                        <img src={modernDesign} alt="Black & white modern living room"></img>
                         <span className="text-container">
                             <h2>Modern Design</h2>
                             <p>Less is more</p>
                         </span>
                     </li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={reading}></img>
+                        <img src={reading} alt="Books stacked face up"></img>
                         <span className="text-container">
                             <h2>Reading</h2>
                             <p>Favorite book: Les Miserables</p>
                         </span>
                     </li>
                     <li className="flex-item scrollmagic-circle">
-                        <img src={dubstep}></img>
+                        <img src={dubstep} alt="Close-up of a subwoofer"></img>
                         <span className="text-container">
                             <h2>Dubstep</h2>
                             <p>Favorite artists:<br/>Kromestar<br/>Von D<br/>Skream<br/>Distance</p>
