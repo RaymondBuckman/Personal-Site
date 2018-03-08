a91b8aa53bd78ae8b1b0f4d53262483590bf32e2
Raymond <dude92128@aol.com>
2018-03-07 23:57:40 -0800

    Update AdditionalSkills.js

--- src/components/additionalSkills/AdditionalSkills.js
+++ src/components/additionalSkills/AdditionalSkills.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class AdditionalSkills extends Component {
+export default class AdditionalSkills extends React.Component {
     render() {
         return(
         <div>
